05dfc698bb5af7dfdd5691036c6bdd8d7aeaa99b
Maliina9 <Maliina9991@outlook.com>
2024-12-12 19:34:12 +0100

    sf

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const server = express();
+const sqlite3 = require('sqlite3').verbose();
+
+server.use(express.json()).use(express.urlencoded({ extended: false}));
 
 server
     .use(express.json())
@@ -10,13 +13,22 @@ server
         res.header('Access-Control-Allow-Methods', '*');
 
         next();
-    })
+    });
 
-server.get('/', (req, res) => {
-    const method = req.method;
-    const url = req.url;
-    res.send(`Du gjorde en ${method} - förfrågan till url:en ${url}`);
-});
+    server.get('/users', (req, res) => {
+        const db = new sqlite3.Database('gik339-labb2.db');
+        const sql = 'SELECT * FROM users';
+        
+        db.all(sql, (err, rows) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.send(rows);
+            }
+        });
+    
+        db.close();
+    });
 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
