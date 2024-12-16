422d33dc91072c75eb9bbd93649c44d2b4d287a9
AkeDalmans <h23akeda@du.se>
2024-12-16 07:08:57 +0100

    Uppgift 7
    La till profilbild

--- client/script.js
+++ client/script.js
@@ -7,8 +7,23 @@ async function getData() {
     const users = await response.json();
     console.log(users);
 
+    const container = document.getElementById("container");
+    if (container) {
+      users.forEach(user => {
+        const html = `
+          <div class="card">
+            <img src="profile.png" alt="${user.firstName}" style="box-shadow: 5px -10px 20px ${user.color}">
+            <h2 style="box-shadow: 0 0 20px ${user.color}">${user.firstName} ${user.lastName}<br>
+            <span class="username">${user.username}</span></h2>
+            </div>`
+        container.insertAdjacentHTML("beforeend", html);
+      });
+    } else {
+      console.error("Container element hittades inte.");
+    }
+
   } catch (error) {
-    console.log("Something went wrong: "+error);
+    console.log("Något gick fel: "+error);
   }
 }
 
